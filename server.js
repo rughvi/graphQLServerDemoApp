cf4ff7ef115f8e2730f78dd68e384597eef065e5
rughvi <rughvi8784@gmail.com>
2022-04-20 22:15:06 +0100

    https for localhost

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const https = require("https");
+const fs = require("fs");
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const graphql = require('graphql');
@@ -11,4 +13,17 @@ app.use('/api', graphqlHTTP({
   schema: schemas.schema,
   graphiql: true,
 }));
-app.listen(4000);
+// app.listen(4000);
+https
+  .createServer(
+		// Provide the private and public key to the server by reading each
+		// file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(4000, () => {
+    console.log("serever is runing at port 4000");
+  });
